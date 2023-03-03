b3be00f1b2164b24e2bc81aa91098b2ae73b25f7
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-03-03 06:13:17 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const width = (num, moth, myage) => {
+    return (num + moth + myage) / 3;
+}
+console.log(String('jamoalar ballarining yigindisi'));
+console.log(width(80, 20, 150));
+console.log(String('birinchi jamoa'));
+console.log(width(90, 80, 170));
+console.log(String('ikkinchi jamoa'));
+console.log(width(90, 88, 160));
+const jamoa1 = 'birinchi jamoa golib chiqdi';
+console.log(jamoa1);
